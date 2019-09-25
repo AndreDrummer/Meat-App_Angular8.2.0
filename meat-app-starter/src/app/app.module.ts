2ca19f006ec080679fce0bfc3b3737b7a7977e70
André Felipe <anprofelipe@gmail.com>
2019-09-25 17:45:56 -0300

    Até aula 77

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import { HomeComponent } from './home/home.component'
@@ -17,22 +16,16 @@ import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurants/reviews/reviews.component';
-import { FormsModule } from '@angular/forms';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart-service';
 // Import necessário para padronização de moeda do Brasil.
 import { registerLocaleData } from '@angular/common';
 import  localePt  from '@angular/common/locales/br';
 import  Pt  from '@angular/common/locales/br';
 import { LOCALE_ID } from '@angular/core';
-import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
-import { StarsComponent } from './shared/stars/stars.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from './shared/sharedModule';
 
 registerLocaleData(localePt, 'pt-BR');
 
@@ -40,7 +33,6 @@ registerLocaleData(localePt, 'pt-BR');
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     HomeComponent,
@@ -49,20 +41,12 @@ registerLocaleData(localePt, 'pt-BR');
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderComponent,
-    InputComponent,
-    RadioComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
-    OrderItemsComponent,
-    OrderSummaryComponent,
-    StarsComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule,
+    SharedModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [RestaurantsService, ShoppingCartService, OrderService],
--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurants/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
@@ -18,8 +16,9 @@ export const ROUTES: Routes = [
     { path: 'reviews', component: ReviewsComponent }
   ]},
   { path: 'order-summary', component: OrderSummaryComponent },
-  { path: 'order', component: OrderComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'order', loadChildren: './order/order.module#OrderModule' },
+  { path: 'about', loadChildren: './about/about.module#AboutModule' }, // Chama-se carregamento tardio, ou
+  // lazy load. Isso significa que o módulo about só será carregado quando requisitado.
 ];
 // Se não colocar isso, assim que abrir o details nenhum componente filho será mostrado.
 // Isso define que menu sera o default assim que abrir o detail
